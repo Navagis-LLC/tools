490af3b844a296a67d461b4c537c7eeda60ae43e
Antonio <antonio.comique@navagis.com>
2020-08-27 09:35:56 +0800

    Adding the InternalTool.

---
+++ NavagisInternalToolSolution/NavagisInternalTool/Credentials/AppFlowMetadata.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web.Configuration;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Mvc;
+using Google.Apis.Util.Store;
+using Google.Apis.CloudResourceManager.v1;
+
+using NavagisInternalTool.Models;
+
+namespace NavagisInternalTool.Credentials
+{
+    public class AppFlowMetadata : FlowMetadata
+    {
+        /*
+         * Credential 
+         */
+        private static string _clientID = new Setting().ClientId;
+        private static string _clientSecret = new Setting().ClientSecret;
+
+        private static readonly IAuthorizationCodeFlow flow =
+            new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = _clientID,
+                    ClientSecret = _clientSecret
+                },
+                Scopes = new[] 
+                {
+                    CloudResourceManagerService.Scope.CloudPlatformReadOnly,
+                    CloudResourceManagerService.Scope.CloudPlatform
+                },
+                DataStore = new FileDataStore("NavagisInternalTool")
+            });
+
+        public override string GetUserId(Controller controller)
+        {
+            var user = controller.Session["user"];
+            if (user == null)
+            {
+                user = Guid.NewGuid();
+                controller.Session["user"] = user;
+            }
+            return user.ToString();
+        }
+
+        public override IAuthorizationCodeFlow Flow
+        {
+            get { return flow; }
+        }
+    }
+}
---
+++ NavagisInternalToolSolution/NavagisInternalTool/Models/Setting.cs
@@ -0,0 +1,29 @@
+﻿namespace NavagisInternalTool.Models
+{
+    public class Setting
+    {
+        public string ClientId
+        {
+            get
+            {
+                return "Please.Update.this.statement.with.proper.clientID.apps.googleusercontent.com";
+            }
+        }
+
+        public string ClientSecret
+        {
+            get
+            {
+                return "Please.update.this.with.proper.clientScret";
+            }
+        }
+
+        public string BillingAccountName
+        {
+            get
+            {
+                return "billingAccounts/{yourbilling.here}";
+            }
+        }
+    }
+}
